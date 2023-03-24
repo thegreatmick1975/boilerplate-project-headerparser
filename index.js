a45299d857917f286fc5499bce8026a1d99d5942
thegreatmick1975 <lkennethwells@gmail.com>
2023-03-24 05:02:47 -0500

    headerparser project

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  console.log("1", req.connection.remoteAddress);
+
+  res.json({ ipaddress: req.connection.remoteAddress, language: req.headers["accept-language"],
+  software: req.headers["user-agent"]
+ });
 });
 
 // listen for requests :)
